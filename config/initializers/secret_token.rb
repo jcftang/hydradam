2370df6ebefb2469627dfc2df1cfda8647349bb1
Chris Beer <chris_beer@wgbh.org>
2012-02-14 14:17:15 -0500

    rails app skeleton

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Hydradam::Application.config.secret_token = '5a52aacdf2232926c5241b13ff7770d96bdf9be808b43c34f8596cf2f1e9b7171a5469ee4245f85e013be3c2325dd845a8bcee07022e4eeaa252a622fc11cef5'
