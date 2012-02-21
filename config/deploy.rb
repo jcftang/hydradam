11995a8370f3d6e076fd631ad685946daec04328
Chris Beer <chris_beer@wgbh.org>
2012-02-21 16:34:08 -0500

    deploy steps

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,22 +1,55 @@
-set :application, "set your application name here"
-set :repository,  "set your repository location here"
+set :application, "hydradam"
+set :deploy_to, "/var/www/#{application}"
+set :domain, 'vagrant'
 
-set :scm, :subversion
-# Or: `accurev`, `bzr`, `cvs`, `darcs`, `git`, `mercurial`, `perforce`, `subversion` or `none`
+default_run_options[:pty] = true
+ssh_options[:forward_agent] = true
+set :user, 'vagrant'
 
-role :web, "your web-server here"                          # Your HTTP server, Apache/etc
-role :app, "your app-server here"                          # This may be the same as your `Web` server
-role :db,  "your primary db-server here", :primary => true # This is where Rails migrations will run
-role :db,  "your slave db-server here"
+set :scm, :git
+set :repository, "git://github.com/WGBH/hydradam.git"
+set :branch, "master"
+set :deploy_via, :remote_cache
+set :git_enable_submodules, 1
+
+$:.unshift(File.expand_path('./lib', ENV['rvm_path']))
+require "rvm/capistrano"  
+require 'bundler/capistrano'
+set :rvm_ruby_string, '1.9.3@hydradam'
+
+
+#set :user, "deployer"
+#set :scm_passphrase, "p@ssw0rd"
+
+role :web, domain                          # Your HTTP server, Apache/etc
+role :app, domain                          # This may be the same as your `Web` server
+role :db,  domain, :primary => true                   # This is where Rails migrations will run
+role :jetty, domain
+
+server domain, :app, :web, :db, :fedora
 
 # if you're still using the script/reaper helper you will need
 # these http://github.com/rails/irs_process_scripts
 
-# If you are using Passenger mod_rails uncomment this:
-# namespace :deploy do
-#   task :start do ; end
-#   task :stop do ; end
-#   task :restart, :roles => :app, :except => { :no_release => true } do
-#     run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
-#   end
-# end
+#If you are using Passenger mod_rails uncomment this:
+namespace :deploy do
+desc "Compile asets"
+  task :assets do
+    run "cd #{release_path}; RAILS_ENV=development bundle exec rake assets:clean assets:precompile"
+  end
+  
+
+end
+
+namespace :passenger do
+  desc "Restart Application"  
+  task :restart do  
+    run "touch #{current_path}/tmp/restart.txt"  
+  end
+end
+
+
+after :deploy, "passenger:restart"
+
+after :deploy, "deploy:assets"
+
