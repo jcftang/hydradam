2582363fcd4d89c936ff01b9f7689444af3e9884
Chris Beer <chris_beer@wgbh.org>
2012-03-13 10:43:28 -0400

    add mime-type specific media partials

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -9,5 +9,22 @@ module ApplicationHelper
     # finally, a controller-specific path for non-catalog subclasses
     @partial_path_templates ||= ["%2$s/%1$s", "%s_%s", "%s_default", "catalog/%s_%s", "catalog/%s_default"]
   end
+
+  def render_media_partial doc, locals = {}
+    format = document_partial_name(doc)
+    mime = doc.get(:mime_type_t).parameterize("_")
+
+    partial_paths = ["%2$s/_media_partials/%1$s", "%2$s/_media_partials/default"]
+
+    partial_paths.each do |str|
+      begin
+        return render :partial => (str % [mime, format]), :locals=>locals.merge({:document=>doc})  
+      rescue ActionView::MissingTemplate 
+        nil
+      end
+    end
+
+    return
+  end
   
 end
