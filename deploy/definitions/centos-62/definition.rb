4157269a0e92d74af4a4fd8fd9bc2496a94289c2
Chris Beer <chris_beer@wgbh.org>
2012-03-19 13:35:22 -0400

    make basebox single-core..

--- deploy/definitions/centos-62/definition.rb
+++ deploy/definitions/centos-62/definition.rb
@@ -1,5 +1,5 @@
 Veewee::Session.declare({
-  :cpu_count => '2', :memory_size=> '2048',
+  :cpu_count => '1', :memory_size=> '2048',
   :disk_size => '100140', :disk_format => 'VDI', :hostiocache => 'off',
   :os_type_id => 'RedHat_64',
   :iso_file => "CentOS-6.2-i386-minimal.iso", :iso_src => "http://mirror.cisp.com/CentOS/6.2/isos/i386/CentOS-6.2-i386-minimal.iso", :iso_md5 => "cc4fbd16bd305f5bf6731b4b10f8fd18", :iso_download_timeout => 1000,
