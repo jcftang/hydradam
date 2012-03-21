9bda2038668cce475ec9fcf70fb1dd9b7773b5e0
Chris Beer <chris_beer@wgbh.org>
2012-03-21 15:17:20 -0400

    move download + extract tasks upstream

--- deploy/puppet/manifests/servicemix.pp
+++ deploy/puppet/manifests/servicemix.pp
@@ -1,55 +1,5 @@
 class servicemix {
-  define download_file(
-        $from="",
-        $cwd="") {                                                                                         
 
-    exec { $name:                                                                                                                     
-        command  => "curl -o ${cwd}/${name} ${from}",                                                         
-        cwd      => $cwd,
-        path     => "/sbin:/bin:/usr/sbin:/usr/bin",
-        creates  => "${cwd}/${name}",                                                              
-    }
-
-    file { "${cwd}/${name}":
-      mode    => 644,
-      require => Exec["${name}"]
-    }
-
-    if $require {
-      Exec[$name] {
-        require +> $require
-      }
-    }
-  }
-
-  define extract_file(
-    $destdir="",
-    $creates="",
-    $user="",
-    $to
-  ) {
-    exec { $name:
-      command => "tar -xvz -C ${destdir} -f ${name}",
-      path    => "/usr/bin:/bin",
-      user    => "${user}",
-      creates => "${destdir}/${creates}",                                                              
-    }
-
-    file { "${destdir}/${creates}":
-      mode    => 775,
-      require => Exec["${name}"]
-    }
-
-    exec { "/bin/mv ${destdir}/${creates} ${to}":
-      require => File["${destdir}/${creates}"],
-      creates => "${to}"
-    }
-
-    file { "${to}":
-      require => Exec["/bin/mv ${destdir}/${creates} ${to}"]
-    }
-
-  }
 
   download_file { "apache-servicemix-fuse.tar.gz":
     from => "http://repo.fusesource.com/nexus/content/repositories/releases/org/apache/servicemix/apache-servicemix/4.4.1-fuse-02-05/apache-servicemix-4.4.1-fuse-02-05.tar.gz",
