c6420bb453538e443333f35313c8059cd2231746
Jimmy Tang <jtang@tchpc.tcd.ie>
2012-07-13 12:20:50 +0100

    force install without user input

--- deploy/definitions/centos-62/postinstall.sh
+++ deploy/definitions/centos-62/postinstall.sh
@@ -3,13 +3,13 @@
 
 date > /etc/vagrant_box_build_time
 
-yum -y install gcc make gcc-c++ kernel-devel-`uname -r` zlib-devel openssl-devel readline-devel sqlite-devel perl path readline zlib libyaml-devel libffi-devel automake bison iconv-devel autoconf automake bzip2 git rsync
+yum -y install gcc make gcc-c++ kernel-devel-`uname -r` zlib-devel openssl-devel readline-devel sqlite-devel perl path readline zlib libyaml-devel libffi-devel automake bison iconv-devel autoconf automake bzip2 git rsync libtool patch
 
 curl https://raw.github.com/wayneeseguin/rvm/master/binscripts/rvm-installer | bash -s stable
 
 source /etc/profile.d/rvm.sh
 
-rvm install ruby-1.9.3
+rvm --force reinstall ruby-1.9.3
 
 rvm --default use 1.9.3
 
